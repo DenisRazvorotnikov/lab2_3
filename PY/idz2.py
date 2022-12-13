f16f773df89f7c657c8a8f1ad2666292c8fb6ce0
DenisRazvorotnikov <deniska_raz26@mail.ru>
2022-12-13 12:23:03 +0300

    Add doc2

--- PY/idz2.py
+++ PY/idz2.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     s = input()
     a = list(s)
     if len(a) % 2 == 0:
-        a[0], a[1] = s[1].upper(), s[0].lower()
-        a[2], a[3] = s[3].upper(), s[2].lower()
+        a[0], a[1] = s[1], s[0]
+        a[2], a[3] = s[3], s[2]
         print(''.join(a))
     else:
         print("число нечетное")
